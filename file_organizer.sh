acd130f5aa1b460d529b2aab5b024e404ede18ec
1998shehab <shehabemad3333@gmail.com>
2024-12-09 20:15:30 +0200

    file orgaizer

---
+++ file_organizer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash/
+# create the directories
+mkdir -p pictures documents others
+for file in * ; do
+  if [ -f  "$file" ] ; then
+    case "$file" in 
+      *.png | *.jpg ) mv "$file" pictures/ ;;
+      *.txt | *.pdf ) mv "$file" documents/ ;;
+      *  ) mv "$file" others/ ;;
+    esac
+  fi
+done 
+
+echo " files organized is specic folders "
